8608cfcbf758b382be4423ed9bc387efdb691442
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2022-03-23 19:34:32 -0300

    Versionando exemplos aula

---
+++ Aula.15.03.2022/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula.Entidade
+{
+    class Pessoa
+    {
+        public string Nome { get; set; }
+
+        public Pessoa(string nome)
+        {
+            Nome = nome ??
+                throw new ArgumentNullException(nameof(nome));
+        }
+    }
+}
---
+++ Aula.15.03.2022/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using E = Aula.Entidade;
+
+namespace Aula._15._03._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Tipo de Valor
+
+            //string nome1 = "Juliano";
+            //string nome2 = nome1;
+
+            //nome1 = "João";
+
+            //Console.WriteLine($"{nameof(nome1)}:{nome1}");
+            //Console.WriteLine($"{nameof(nome2)}:{nome2}");
+
+            //Console.ReadLine();
+
+            #endregion Tipo de Valor
+
+            #region Tipo de Referencia
+
+            //E.Pessoa pessoa = new E.Pessoa()
+            //{
+            //    Nome = "Juliano",
+            //};
+
+            //pessoa.Nome = "João";
+
+            //E.Pessoa pessoa = new E.Pessoa(null);
+
+            //E.Pessoa pessoa1 = new E.Pessoa("Juliano");
+            //E.Pessoa pessoa2 = pessoa1;
+
+            //pessoa1.Nome = "João";
+
+            E.Pessoa pessoa1 = new E.Pessoa("Juliano");
+            E.Pessoa pessoa2 = pessoa1;
+
+            pessoa1 = new E.Pessoa("João");
+            pessoa2 = pessoa1;
+
+            Console.WriteLine($"{nameof(pessoa1)}:" +
+                $"{pessoa1.Nome}");
+
+            Console.WriteLine($"{nameof(pessoa2)}:" +
+                $"{pessoa2.Nome}");
+
+            Console.ReadLine();
+
+            #endregion Tipo de Referencia
+        }
+    }
+}
